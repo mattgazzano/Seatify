cbb1a10684d965c3cea9ca030334140141385a99
Matthew Gazzano <mattgazzano@gmail.com>
2022-11-03 22:21:09 -0400

    Update seatify_dashboard.py

--- visualization/seatify_dashboard.py
+++ visualization/seatify_dashboard.py
@@ -2,10 +2,10 @@ import streamlit as st
 from PIL import Image
 
 header = st.container()
-seatify_logo = Image.open('seatify_logo.jpg')
+seatify_logo = Image.open('seatify_logo.png')
 
-st.image(seatify_logo)
 
 with header:
+    st.image(seatify_logo)
     st.title('Seatify')
     st.subheader('Bringing together Spotify and Seatgeek data from publicly available APIs')
