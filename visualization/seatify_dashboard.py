bcf0a835368ded1599e761eb823924d360c5a127
Matthew Gazzano <mattgazzano@gmail.com>
2022-11-03 22:21:38 -0400

    Update seatify_dashboard.py

--- visualization/seatify_dashboard.py
+++ visualization/seatify_dashboard.py
@@ -4,8 +4,8 @@ from PIL import Image
 header = st.container()
 seatify_logo = Image.open('seatify_logo.png')
 
+st.image(seatify_logo)
 
 with header:
-    st.image(seatify_logo)
     st.title('Seatify')
     st.subheader('Bringing together Spotify and Seatgeek data from publicly available APIs')
