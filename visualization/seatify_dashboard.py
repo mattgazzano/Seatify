38d4170d219b817264b467816cbc9ccd5ea7630e
Matthew Gazzano <mattgazzano@gmail.com>
2022-11-03 22:35:07 -0400

    Update seatify_dashboard.py

--- visualization/seatify_dashboard.py
+++ visualization/seatify_dashboard.py
@@ -1,11 +1,29 @@
 import streamlit as st
 from PIL import Image
 
+
+# Page Title
+st.set_page_config(page_title='Seatify', page_icon=':chart_with_upwards_trend:',layout='wide')
+
+
+# Header Section
 header = st.container()
 seatify_logo = Image.open('seatify_logo.png')
+# st.image(seatify_logo)
 
-st.image(seatify_logo)
 
 with header:
     st.title('Seatify')
-    st.subheader('Bringing together Spotify and Seatgeek data from publicly available APIs')
+    st.write('''
+    Both [Spotify](https://developer.spotify.com/documentation/web-api/) and [SeatGeek](https://platform.seatgeek.com/?ref=publicapis.dev) 
+    offer open source API’s to access Artists and Event data respectively. What's unique with using both of these sources is that SeatGeek provides a 
+    [Spotify Artist ID in the Performers object](https://platform.seatgeek.com/?ref=publicapis.dev#performers) which therefore allows you to connect 
+    data between the two platforms. In doing this, we can draw many conclusions on popular Artists, such as understanding the total number of listens 
+    they are receiving on their songs, and how many shows they are performing this year.
+
+    The goal of this project is to create a full stack analytics project that connects both of these sources via their API’s, 
+    transform their raw data into a usable star-schema inside of a Postgres database, and present it in a meaningful way.
+    
+    You can read more about the architechture of the project on [Github](https://github.com/mattgazzano/seatify).
+    '''
+    )
