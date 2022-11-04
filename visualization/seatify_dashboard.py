4b85e1735df015958db620d99a08565a90202e7f
Matthew Gazzano <mattgazzano@gmail.com>
2022-11-03 22:08:53 -0400

    Update seatify_dashboard.py

---
+++ visualization/seatify_dashboard.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+header = st.beta_container()
+
+with header:
+    st.title('Seatify')
+    st.subheader('Brining together Spotify and Seatgeek data from publicly available APIs')
