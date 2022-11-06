cf5399e57c40df390201414a20a2f482109bad01
Matthew Gazzano <mattgazzano@gmail.com>
2022-11-06 17:36:06 -0500

    Update Requirements

--- visualization/seatify_dashboard.py
+++ visualization/seatify_dashboard.py
@@ -1,13 +1,32 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
-import gspread
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["seatify_dashboard_postgres_tables"]
+rows = run_query(f'SELECT * FROM "{sheet_url}" LIMIT 50')
 
 # Page Title
 st.set_page_config(page_title='Seatify', page_icon=':chart_with_upwards_trend:',layout='wide')
 
 # Header Section
-st.image(Image.open('/visualization/seatify_logo.jpg'), width=400)
+st.image(Image.open('/visualization/assets/seatify_logo.jpg'), width=400)
 st.title('Seatify')
 st.subheader('by [Matthew Gazzano](https://www.linkedin.com/in/matthewgazzano/)')
 st.write('''
@@ -27,4 +46,4 @@ gcloud_service_account = gspread.service_account()
 seatify_dashboard_postgres_tables = gcloud_service_account.open_by_key(st.secrets['seatify_dashboard_postgres_tables']).worksheet('dim_artists')
 df_artists = pd.json_normalize(seatify_dashboard_postgres_tables.get_all_records())
 
-st.table(df_artists.head(50))
+st.table(rows)
